2f38c723ddd849736bbb686bbee6eeaae2bcf7ef
chanjeff520 <chanjeff520@gmail.com>
2022-09-27 22:18:25 -0400

    add a prompt to ask for length of char and checks if char length is valid

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var password;
+
+function generatePassword(){
+  //Variable need for this
+  var characterCount;
+  var isTrue;
+
+  //prompt the user for the length of the password
+  characterCount=prompt("How many characters for your password(must be 8 to 1024 characters)?");
+
+  //checks if the password is the password have right amount of characters
+  if(characterCount < 8 || characterCount > 1024){
+    alert("You need at least 8 characters!!");
+    return;
+  }else if(characterCount > 1024){
+    alert("You have more than 1024 characters!!")
+    return;
+  }
+
+
+};
